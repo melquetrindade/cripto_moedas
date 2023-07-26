94ae1fdac4dac2d7aa7eb7bedba582a94d63e597
melquetrindade <melquetrindade654@gmail.com>
2023-07-26 11:29:42 -0300

    finalizando a primeira parte da aula 2 do curso de flutter (cripto moedas)

---
+++ cripto/lib/repositories/moeda_repository.dart
@@ -0,0 +1,36 @@
+import '../models/moeda.dart';
+
+class MoedaRepository {
+  List<Moeda> tabela = [
+    Moeda(
+      icone: 'images/bitcoin.png', 
+      nome: 'Bitcoin', 
+      sigla: 'BTC', 
+      preco: 138647.54),
+    Moeda(
+      icone: 'images/ethereum.png', 
+      nome: 'Ethereum', 
+      sigla: 'ETH', 
+      preco: 8797.56),
+    Moeda(
+      icone: 'images/xrp.png', 
+      nome: 'XRP', 
+      sigla: 'XRP', 
+      preco: 3.33),
+    Moeda(
+      icone: 'images/cardano.png', 
+      nome: 'Cardano', 
+      sigla: 'ADA', 
+      preco: 1.44),
+    Moeda(
+      icone: 'images/litecoin.png', 
+      nome: 'Litecoin', 
+      sigla: 'LTC', 
+      preco: 424.72),
+    Moeda(
+      icone: 'images/usdcoin.png', 
+      nome: 'USD Coin', 
+      sigla: 'USDC', 
+      preco: 26.64),
+  ];
+}
